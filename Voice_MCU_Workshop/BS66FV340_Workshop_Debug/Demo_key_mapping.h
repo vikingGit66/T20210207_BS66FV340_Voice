71027c6be32d6c7e6c7a36c7611ef77fe556f2b2
vikingGit66 <2501226760@qq.com>
2021-02-07 10:08:56 +0800

    master

---
+++ Voice_MCU_Workshop/BS66FV340_Workshop_Debug/Demo_key_mapping.h
@@ -0,0 +1,7 @@
+#include bs66fv340.inc
+
+;****************************************
+;COMMAND TRIGGER DEFINE
+; HOW MANY COMMAND BE DEFINE
+;****************************************
+#define COMMANDABLE_NUM  0  ;;
